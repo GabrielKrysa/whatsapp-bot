265b4aca92f0a264fe8f89bad4d8f5f566491e5a
Gabriel Krysa <gabrielkrysa@gmail.com>
2019-08-12 23:44:58 -0300

    Implementada a função de envio de mensagem

--- main.js
+++ main.js
@@ -1,19 +1,74 @@
-// Modules to control application life and create native browser window
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
+const { app, BrowserWindow, session } = require('electron')
+const ipc = require('electron').ipcMain
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let win
+let mainWindow
 
-app.on('ready', function() {
-    win = new BrowserWindow({ width: 800, height: 600 });
-    var phone = "554299524440";
-    var message = "BLA BLA BLA"
-    win.loadURL('https://web.whatsapp.com/send?php' + phone + '$text=' + message, { userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36' });
-    win.webContents.executeJavaScript('var input = document.getElementsByClassName("_3u328")[0];    var send = document.getElementsByClassName("_3M-N-")[0]; if (typeof input !== "undefined" && input.textContent) {    send.click();}  ');
-
-    win.on('closed', () => {
-        win = null
+function createWindow() {
+    session.defaultSession.webRequest.onBeforeSendHeaders((details, callback) => {
+        details.requestHeaders['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 OPR/57.0.3098.116';
+        callback({ cancel: false, requestHeaders: details.requestHeaders });
     });
-});
+
+    mainWindow = new BrowserWindow({
+        width: 1280,
+        height: 720,
+        webPreferences: {
+            nodeIntegration: false,
+            contextIsolation: false,
+        }
+    })
+
+    let number = '554299524440'
+    let msg = 'blablabla'
+
+    mainWindow.loadURL('https://web.whatsapp.com/send?phone=' + number + '&text=' + msg);
+
+    mainWindow.webContents.on('did-finish-load', function() {
+        mainWindow.webContents.executeJavaScript(`console.log('Website loaded succesfully')`)
+        mainWindow.webContents.executeJavaScript(`console.log('ABRIU')`)
+        sleep(100);
+        mainWindow.webContents.executeJavaScript('function time() { var btnsend = document.getElementsByClassName("_3M-N-")[0]; btnsend.click(); } setInterval(time, 5000);');
+    });
+
+
+    mainWindow.webContents.openDevTools()
+
+
+    mainWindow.on('closed', function() {
+        mainWindow = null
+    })
+
+    ipc.on('whatsapp-is-ready', function(event, args) {
+        mainWindow.webContents.send('bot-ready')
+    })
+
+    mainWindow.webContents.on('whatsapp-is-ready', function(event, args) {
+        mainWindow.webContents.send('bot-ready')
+    })
+
+    function sleep(milliseconds) {
+        var start = new Date().getTime();
+        for (var i = 0; i < 1e7; i++) {
+            if ((new Date().getTime() - start) > milliseconds) {
+                break;
+            }
+        }
+    }
+}
+
+app.on('ready', createWindow)
+
+app.commandLine.appendSwitch('autoplay-policy', 'no-user-gesture-required');
+
+app.on('window-all-closed', function() {
+
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', function() {
+    if (mainWindow === null) {
+        createWindow()
+    }
+})
--- verifySend.js
+++
@@ -1,6 +0,0 @@
-var input = document.getElementsByClassName("_3u328")[0];
-var send = document.getElementsByClassName("_3M-N-")[0];
-
-if (typeof input !== "undefined" && input.textContent) {
-    send.click();
-}
